2bb5bb92376461fdd187002332165f54d3c510d7
Damian Czernikowski <dczernikowski1@ucvts.tec.nj.us>
2020-03-11 12:42:17 -0400

    first commit

--- Calculator.java
+++ Calculator.java
@@ -5,9 +5,14 @@ public class Calculator {
     public double tax = .05;
     public double tip = .15;
     
-    public void findTotal(String person, double price){
+    public double findExtra(String person, double price) {
         double total = price*(1+tax+tip);
+        return total;
+    }
+    
+    public double findTotal(String person, double price, double extra){
+        double total = (price*(1+tax+tip)) + extra;
         System.out.println(person + " $" + total);
-
+        return total;
     }
 }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -3,20 +3,18 @@ package tip03;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        
-       Calculator calc = new Calculator();
        
+       Calculator calc = new Calculator();
+       double extra = (calc.findExtra("Person 6", 15) + calc.findExtra("Person 8", 30))/6;
        //Use the Calculator object and arguments supplied to findTotal()
        //to print the total for each person
-       calc.findTotal("Person 1",10);
-       calc.findTotal("Person 2",12);
-       calc.findTotal("Person 3",9);
-       calc.findTotal("Person 4",8);
-       calc.findTotal("Person 5",7);
-       calc.findTotal("Person 6",15);
-       calc.findTotal("Person 7",11);
-       calc.findTotal("Person 8",30);
-       
+       System.out.println( + calc.findTotal("Person 1",10,extra) +
+               calc.findTotal("Person 2",12,extra) +
+               calc.findTotal("Person 3",9,extra) +
+               calc.findTotal("Person 4",8,extra) +
+               calc.findTotal("Person 5",7,extra) +
+               calc.findTotal("Person 7",11,extra));
+      
        /*This is what everyone owes before tax and tip:
        Person 1: $10
        Person 2: $12
